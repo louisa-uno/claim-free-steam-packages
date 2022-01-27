29f17798834936058dfdfaa8c2447e9d5abed69e
Luois45 <github@louis45.de>
2022-01-27 03:45:21 +0100

    Added a status bar

--- activate_packages.py
+++ activate_packages.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 from ASF import IPC
+from tqdm import tqdm
 
 
 async def command(asf, cmd):
@@ -14,28 +15,28 @@ async def main():
 	               password='YOUR IPC PASSWORD') as asf:
 		with open('package_list.txt', 'r') as f:
 			apps = f.read().split(',')
-		for app in apps:
+		for app in tqdm(apps, desc='Activating licenses'):
 			try:
 				with open('activated_packages.txt', 'r') as f:
 					aps = f.read().split(',')
 			except FileNotFoundError:
 				with open('activated_packages.txt', 'w') as f:
-					print("Created activated_packages file")
+					print("\nCreated activated_packages file")
 					aps = []
 			foundPackage = False
 			for ap in aps:
 				if app == ap:
-					print("Package found in activated_packages")
+					# print("\nPackage found in activated_packages")
 					foundPackage = True
 			if not foundPackage:
-				print("Package not found in activated_packages")
+				# print("\nPackage not found in activated_packages")
 				cmd = "!addlicense app/" + app
 				activatedPackage = False
 				tries = 10
 				for i in range(tries):
 					resp = await command(asf, cmd)
 					if resp.success:
-						print(resp.result)
+						print("\n" + resp.result)
 						successCodes = ["Items:", "Aktivierte IDs:"]
 						if any(x in resp.result for x in successCodes):
 							activatedPackage = True
@@ -44,8 +45,8 @@ async def main():
 							time.sleep(20)
 							break
 					else:
-						print(f'Error: {resp.message}')
-					time.sleep(20)
+						print(f'\nError: {resp.message}')
+					time.sleep(60)
 				if not activatedPackage:
 					time.sleep(20)
 
