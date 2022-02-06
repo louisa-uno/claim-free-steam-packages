2ee3348b4f890ac5281f1a735edc86e60d4e06c9
Luois45 <github@louis45.de>
2022-02-06 04:52:45 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-del activate_packages.exe
-del activate_packages-headless.exe
+del dist\activate_packages.exe
+del dist\activate_packages-headless.exe
 pyinstaller activate_packages.py -F -n activate_packages
 pyinstaller activate_packages.py -F -n activate_packages-headless -w
