b04b89a95ac0d12894a7ea03a9032fb8f733a441
Luois45 <github@louis45.de>
2022-02-09 16:40:28 +0100

    Fixed bug

--- get_package_list.py
+++ get_package_list.py
@@ -93,10 +93,11 @@ for app in res['applist']['apps']:
 
 print('Received %d apps' % len(apps))
 
-with tqdm_joblib(tqdm(desc='Requesting app statuses',
-                      total=len(apps),
-                      miniters=int(len(apps)/1000))) as progress_bar:
-	results = Parallel(n_jobs=30)(delayed(checkGame)(i) for i in apps,)
+with tqdm_joblib(
+        tqdm(desc='Requesting app statuses',
+             total=len(apps),
+             miniters=int(len(apps) / 1000))) as progress_bar:
+	results = Parallel(n_jobs=30)(delayed(checkGame)(i) for i in apps)
 
 output = ''
 for game in results:
