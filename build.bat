c03dea5d202a2b338e3a522b779e37becbae4b74
Wok <woctezuma@users.noreply.github.com>
2022-11-15 18:08:47 +0100

    Clean-up

--- build.bat
+++
@@ -1,4 +0,0 @@
-del dist\activate_packages.exe
-del dist\activate_packages-headless.exe
-pyinstaller activate_packages.py -F -n activate_packages
-pyinstaller activate_packages.py -F -n activate_packages-headless -w
